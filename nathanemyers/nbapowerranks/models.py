0438925a1a611926dff0a541096f0a7dd2f2544a
Nathan Myers <nathanemyers@gmail.com>
2015-10-22 10:37:38 -0400

    starting django project

---
+++ nathanemyers/nbapowerranks/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Team(models.Model):
+    region = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    conference = models.CharField(max_length=100)
+    def __str__(self):
+        return self.region + ' ' + self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.ForeignKey(Team)
+    def __str__(self):
+        return name
+
+class Ranking(models.Model):
+    year = models.IntegerField()
+    week = models.IntegerField()
+    team = models.ForeignKey(Team)
+    rank = models.IntegerField()
+    summary = models.TextField()
+    def __str__(self):
+        return year + 'week ' + week + ': ' + team + ' #' + rank
+
