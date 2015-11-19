9beed6b589f1f7bb05989db3a4546264822ba7f3
Nathan Myers <nathanemyers@gmail.com>
2015-11-19 18:45:10 -0500

    adding comments

--- nathanemyers/at_mapper/tools/latlngExtract.sh
+++ nathanemyers/at_mapper/tools/latlngExtract.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Attempting to use PIL's EXIF libraries for extracting
+# GPS data is about as fun as pulling teeth. Plus iPhones
+# appear to have a unique way of encoding GPS data that's
+# making extraction difficult. Thankfully OSX comes with a
+# utility 'mdls' that neatly outputs iPhone EXIF data
+
 lat=$(mdls $1 | grep Latitude | awk '{print $3}')
 lng=$(mdls $1 | grep Longitude | awk '{print $3}')
 echo $lat / $lng
